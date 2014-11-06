2aa06c4ec36697391d61d5c2e907f16eb39def98
Pedro Alcântara <pedro.filipi.alcantara@gmail.com>
2014-11-06 17:46:17 -0300

    update

---
+++ lab05/src/lp2/lab05/RoboTest.java
@@ -0,0 +1,93 @@
+package lp2.lab05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoboTest {
+	
+	Sala salinha;
+	int energia;
+	Robo robozinho = new Robo(salinha, energia);
+	
+	@Test
+	public void testRobo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPosicaoInicialRobo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosicaoRobo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAndaFrente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
